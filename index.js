ad493b3cf6222842b1884b3ac7878c042934589f
unknown <emonhossen1000@gmail.com>
2023-11-12 13:01:41 +0600

    server connected

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  
 
     const brandCollection = client.db("digitalDB").collection("brands");
     const brandDetails = client.db("digitalDB").collection("brandDetails");
